cbbcb4f4d5f2286b17ec9697b3fa6d6fc5d0a5e8
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-05-05 13:13:21 -0300

    Tratando erros com excessao personalizada

--- Xadrez/Xadrez/Program.cs
+++ Xadrez/Xadrez/Program.cs
@@ -8,15 +8,20 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta),new Posicao(2, 4));
-
-            Tela.imprimirTabuleiro(tab);
-
-            Console.WriteLine(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroExeption e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
             Console.ReadLine();
         }
     }
