d1b04c7eadc8e2d0e2db09e34c89ec3004b16dc1
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-05-06 09:18:23 -0300

    classe posicao xadrex

--- Xadrez/Xadrez/Program.cs
+++ Xadrez/Xadrez/Program.cs
@@ -8,12 +8,18 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.toPosicao());
+
+            /*
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 4));
 
                 Tela.imprimirTabuleiro(tab);
             }
@@ -21,6 +27,7 @@ namespace Xadrez
             {
                 Console.WriteLine(e.Message);
             }
+            */
             
             Console.ReadLine();
         }
