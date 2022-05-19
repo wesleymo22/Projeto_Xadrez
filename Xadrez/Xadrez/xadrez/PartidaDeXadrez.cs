bb76632cc701443d5403994e7228376db8b210b7
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-05-19 10:39:17 -0300

    Jogada especial promocao

--- Xadrez/Xadrez/xadrez/PartidaDeXadrez.cs
+++ Xadrez/Xadrez/xadrez/PartidaDeXadrez.cs
@@ -144,6 +144,22 @@ namespace xadrez
                 throw new TabuleiroExeption("Voce não pode se colocar em Xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // Promocao
+
+            if(p is Peao)
+            {
+                if (p.cor == Cor.Branca && destino.linha == 0 || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Rainha(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -163,7 +179,7 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             //Jogada especial En Passant
             if (p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha + 2))
